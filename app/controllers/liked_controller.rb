5d45108bdde3f3b169a0bfb310eb48b42dd447ea
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-28 05:48:53 +0300

    Fix like redirect path

--- app/controllers/liked_controller.rb
+++ app/controllers/liked_controller.rb
@@ -13,10 +13,10 @@ class LikedController < ApplicationController
     if check_like(current_user, @template)
       Liked.where(user: current_user, template: @template).first.destroy
       flash[:notice] = "Template was successfully removed from liked."
-      redirect_to liked_index_path
     else
       current_user.likeds.create(user: current_user, template: @template)
-      redirect_to template_path(@template)
     end
+
+    redirect_to request.referer
   end
 end
